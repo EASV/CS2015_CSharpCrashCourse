9b93463320e8bca65dc7318a7df16f144a80e42c
lbilde <larsbilde@gmail.com>
2016-08-08 13:58:16 +0200

    Added CarConsole Project

---
+++ CarConsole/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarConsole.Model
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public int Year { get; set; }
+        public string LicenceNumber { get; set; }
+        public string Color { get; set; }
+
+        public void Drive() {
+            Console.WriteLine("Wrooom");
+        }
+
+        public void Break() {
+            Console.WriteLine("eeehehhhhhhehhhhh");
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Year: ").Append("\t").Append("\t");
+            sb.AppendLine(Year.ToString());
+            sb.Append("Make: ").Append("\t").Append("\t");
+            sb.AppendLine(Make);
+            sb.Append("LicenceNumber: ").Append("\t");
+            sb.AppendLine(LicenceNumber);
+            sb.Append("Color: ").Append("\t").Append("\t");
+            sb.AppendLine(Color);
+            return sb.ToString();
+        }
+    }
+}
---
+++ CarConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarConsole.Model;
+
+namespace CarConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car theCar = new Car() {
+                Color = "Red",
+                LicenceNumber = "333",
+                Make = "Toyota",
+                Year = 2001
+            };
+            
+            Console.WriteLine(theCar.ToString());
+
+            theCar.Drive();
+            theCar.Break();
+            theCar.Drive();
+            theCar.Break();
+
+            Console.ReadLine();
+        }
+    }
+}
