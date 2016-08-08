cf11c0c7ce0a61aba2de3fa7e6ec4530731ae110
lbilde <larsbilde@gmail.com>
2016-08-08 10:38:41 +0200

    Added new project Calculator

---
+++ ConsoleCalculator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type your first number");
+            var firstNumberAsString = Console.ReadLine();
+
+            int firstNumber;
+
+            while (!int.TryParse(firstNumberAsString, out firstNumber)) {
+                Console.WriteLine("Thats not a number, please try again");
+                firstNumberAsString = Console.ReadLine();
+
+            }
+            Console.WriteLine("You wrote the number: " + firstNumber);
+
+            Console.WriteLine("Type your second number");
+            var secondNumberAsString = Console.ReadLine();
+
+            int secondNumber;
+
+            while (!int.TryParse(secondNumberAsString, out secondNumber))
+            {
+                Console.WriteLine("Thats not a number, please try again");
+                secondNumberAsString = Console.ReadLine();
+
+            }
+
+            Console.WriteLine("You wrote the number: " + secondNumber);
+
+
+            Console.WriteLine("You wrote the numbers: " + firstNumber + " and " + secondNumber + " added it results in " + (firstNumber + secondNumber));
+
+            Console.ReadLine();
+        }
+    }
+}
