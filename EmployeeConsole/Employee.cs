da4f1d584dc99aba80e8925d488f96adf36a2b32
lbilde <larsbilde@gmail.com>
2016-08-10 11:07:15 +0200

    Employee Console

---
+++ EmployeeConsole/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeConsole
+{
+    
+   /// <summary>
+   /// Remeber to make employee public, so other classes can access it
+   /// </summary>
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int SalaryLevel { get; set; }
+        public string Department { get; set; }
+        public double WeeklyWorkHours { get; set; }
+
+        public double CalculatedSalery {
+            get{ return SalaryLevel * 700.0; }
+        }
+
+        public double GetCalculatedSalery() {
+            return SalaryLevel * 700.0;
+        }
+        
+        public override string ToString()
+        {
+            return $" Name: {Name} \t\t\n Email: {Email} \t\t\n SalaryLevel: {SalaryLevel} \t\n Salary: {GetCalculatedSalery()} \t\n WeeklyWorkHours: {WeeklyWorkHours} \t\n Department: {Department} ";
+        }
+    }
+}
---
+++ EmployeeConsole/Program.cs
@@ -0,0 +1,39 @@
+﻿using EmployeeConsole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee()
+            {
+                Name = "Lars Bilde",
+                Email = "theEmail@Email.com",
+                Department = "The cool place",
+                SalaryLevel = 10000,
+                WeeklyWorkHours = 2
+            };
+
+            Console.WriteLine(employee);
+
+            Console.WriteLine("\n\n\nOnly the salary: " + employee.CalculatedSalery);
+            
+            if (employee.CalculatedSalery > 2000)
+            {
+                Console.WriteLine($"You are filthy rich earning {employee.CalculatedSalery} $");
+            }
+            else
+            {
+                Console.WriteLine("Talk to the boss, u need more money");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
