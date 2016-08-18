2098ff97494d3e2ddee2d89df5b98156b821bf5f
lbilde <larsbilde@gmail.com>
2016-08-18 08:16:31 +0200

    Removed old code

--- PersonManager/Program.cs
+++ PersonManager/Program.cs
@@ -10,37 +10,7 @@ namespace PersonManager
     {
         static void Main(string[] args)
         {
-            List<Person> persons = new List<Person>();
-
             
-
-
-            var person = new Person();
-            person.Name = "Lars Bilde";
-            person.BirthDate = DateTime.Now.AddYears(-25);
-            person.Gender = Gender.Male;
-            persons.Add(person);
-
-            var person2 = new Person() {
-                Name = "Ole Eriksen",
-                BirthDate = DateTime.Now.AddYears(-125),
-                Gender = Gender.Female
-            };
-            persons.Add(person2);
-
-            Console.WriteLine("For loop below:");
-            for (int i = 0; i < persons.Count; i++)
-            {
-                Console.WriteLine(persons[i]);
-            }
-
-            Console.WriteLine("Foreach loop below:");
-            foreach (var item in persons)
-            {
-                Console.WriteLine(item);
-            }
-
-            Console.ReadLine();
         }
     }
 }
